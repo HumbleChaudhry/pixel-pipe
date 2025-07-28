9bb2ee36dacbd9e9ec04f758326784719c0a4d2a
HumbleChaudhry <humblechaudhry@live.ca>
2025-07-27 19:30:58 -0600

    Add IAM role for resize-worker Lambda

--- iam.tf
+++ iam.tf
@@ -97,3 +97,51 @@ resource "aws_iam_role_policy_attachment" "dispatch_tasks_lambda_attach" {
   role       = aws_iam_role.dispatch_tasks_lambda_role.name
   policy_arn = aws_iam_policy.dispatch_tasks_lambda_policy.arn
 }
+
+# --- IAM for resize-worker Lambda ---
+
+resource "aws_iam_role" "resize_worker_lambda_role" {
+  name = "${var.project_name}-resize-worker-role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "resize_worker_lambda_policy" {
+  name   = "${var.project_name}-resize-worker-policy"
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Action   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"],
+        Effect   = "Allow",
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Action   = ["sqs:ReceiveMessage", "sqs:DeleteMessage", "sqs:GetQueueAttributes"],
+        Effect   = "Allow",
+        Resource = aws_sqs_queue.resize_queue.arn
+      },
+      {
+        Action   = "s3:GetObject",
+        Effect   = "Allow",
+        Resource = "${aws_s3_bucket.uploads.arn}/*"
+      },
+      {
+        Action   = "s3:PutObject",
+        Effect   = "Allow",
+        Resource = "${aws_s3_bucket.processed.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "resize_worker_lambda_attach" {
+  role       = aws_iam_role.resize_worker_lambda_role.name
+  policy_arn = aws_iam_policy.resize_worker_lambda_policy.arn
+}
