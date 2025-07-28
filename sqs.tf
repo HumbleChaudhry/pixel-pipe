f64f1223b9f356c1c88330693865eb98e2591aff
HumbleChaudhry <humblechaudhry@live.ca>
2025-07-27 19:26:27 -0600

    Create sqs.tf

---
+++ sqs.tf
@@ -0,0 +1,14 @@
+resource "aws_sqs_queue" "resize_queue" {
+    name = "${var.project_name}-resize-queue"
+    tags = {
+        Name = "${var.project_name}-resize-queue"
+        Project = var.project_name
+    }
+}
+
+#SNS topic linking to SQS queue
+resource "aws_sns_topic_subscription" "resize_queue_subscription" {
+    topic_arn = aws_sns_topic.image_events.arn
+    protocol = "sqs"
+    endpoint = aws_sqs_queue.resize_queue.arn
+}
