82020238b20cc17237db797fcce6f2d13b89e1df
HumbleChaudhry <humblechaudhry@live.ca>
2025-07-27 19:30:01 -0600

    Add IAM role for dispatch-tasks Lambda

--- iam.tf
+++ iam.tf
@@ -59,3 +59,41 @@ resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.lambda_policy.arn
 }
+
+# --- IAM for dispatch-tasks Lambda ---
+
+resource "aws_iam_role" "dispatch_tasks_lambda_role" {
+  name = "${var.project_name}-dispatch-tasks-role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "dispatch_tasks_lambda_policy" {
+  name   = "${var.project_name}-dispatch-tasks-policy"
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Action   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"],
+        Effect   = "Allow",
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Action   = "sns:Publish",
+        Effect   = "Allow",
+        Resource = aws_sns_topic.image_events.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "dispatch_tasks_lambda_attach" {
+  role       = aws_iam_role.dispatch_tasks_lambda_role.name
+  policy_arn = aws_iam_policy.dispatch_tasks_lambda_policy.arn
+}
